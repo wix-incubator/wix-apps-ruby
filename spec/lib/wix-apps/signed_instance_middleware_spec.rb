b28ad6659d5531b571ad9b67d806b4fea23de323
Niklas Bichinger <niklas@bichinger.de>
2016-10-24 00:31:47 +0200

    version 1.0.0 work in progress

--- spec/lib/wix-apps/signed_instance_middleware_spec.rb
+++ spec/lib/wix-apps/signed_instance_middleware_spec.rb
@@ -7,47 +7,44 @@ describe Wix::Apps::SignedInstanceMiddleware do
   let(:secret) { 'd245bbf8-57eb-49d6-aeff-beff6d82cd39' }
 
   let(:middleware) { Wix::Apps::SignedInstanceMiddleware.new(app, secured_paths: ['/wix'],
-                      secret_key: secret) }
+                                                             secret_key: secret) }
   let(:mock_request) { Rack::MockRequest.new(middleware) }
 
   let(:instance) { 'HottEZ2jPjqsqS8sFWwngJDZAc5L6BBv5j5N9WAN0Go.eyJpbnN0YW5jZUlkIjoiYjgxNDBlNGQtNDc1ZC00OGVkLTgxOWYtYmFkMGRlNDQ3MDY5Iiwic2lnbkRhdGUiOiIyMDEyLTA4LTExVDEzOjU2OjQ0LjYzNVoiLCJ1aWQiOm51bGwsInBlcm1pc3Npb25zIjpudWxsfQ' }
-  let(:response) { mock_request.get('/wix', params: { 'instance' => instance }) }
+  let(:response) { mock_request.get('/wix', params: {'instance' => instance}) }
 
-  describe "Unsecured paths" do
+  describe 'Unsecured paths' do
     let(:response) { mock_request.get('/') }
-    it("returns a 200") { response.status.should == 200 }
+    it('returns a 200') { expect(response.status).to eq 200 }
   end
 
-  describe "Secured Paths" do
-    describe "without instance" do
+  describe 'Secured Paths' do
+    describe 'without instance' do
       let(:response) { mock_request.get('/wix') }
-      it("returns a 401") { response.status.should == 401 }
+      it('returns a 401') { expect(response.status).to eq 401 }
     end
 
-    describe "with invalid instance" do
+    describe 'with invalid instance' do
       let(:instance) { 'invalid.instance' }
-      it("returns a 403") { response.status.should == 403 }
+      it('returns a 403') { expect(response.status).to eq 403 }
     end
 
-    describe "with valid instance" do
-      it("returns a 200") { response.status.should == 200 }
+    describe 'with valid instance' do
+      it('returns a 200') { expect(response.status).to eq 200 }
 
-      describe "instance parsing" do
-        it "have instance_id" do
-          app.should_receive(:call) do |arg|
-            arg['rack.request.query_hash']['parsed_instance']['instance_id']
-              .should eq('b8140e4d-475d-48ed-819f-bad0de447069')
-
+      describe 'instance parsing' do
+        it 'have instance_id' do
+          expect(app).to receive(:call) do |arg|
+            expect(arg['wix.instance'].instance_id).to eq 'b8140e4d-475d-48ed-819f-bad0de447069'
             [200, {}, []]
           end
 
           response
         end
 
-        it "have sign_date" do
-          app.should_receive(:call) do |arg|
-            arg['rack.request.query_hash']['parsed_instance']['sign_date']
-              .should eq(DateTime.parse("2012-08-11T13:56:44.635Z"))
+        it 'have sign_date' do
+          expect(app).to receive(:call) do |arg|
+            expect(arg['wix.instance'].sign_date).to eq DateTime.parse('2012-08-11T13:56:44.635Z')
             [200, {}, []]
           end
 
@@ -55,13 +52,12 @@ describe Wix::Apps::SignedInstanceMiddleware do
         end
       end
 
-      describe "logged in user" do
+      describe 'logged in user' do
         let(:instance) { '0jepzq2Gi8zFxLdS_LhTuXIkmFR41H1QOstEtn1v4w0.eyJpbnN0YW5jZUlkIjoiOWY5YzVjMTYtNTljOC00NzA4LThjMjUtODU1NTA1ZGFhOTU0Iiwic2lnbkRhdGUiOiIyMDEyLTA4LTEyVDEwOjA0OjE3Ljg1MloiLCJ1aWQiOiIyOWQ4MjA0YS0zYjgyLTRhOTgtOGQ4Ni0yNDY0YTZiODM2ZGEiLCJwZXJtaXNzaW9ucyI6bnVsbH0' }
 
-        it "have user_id" do
-          app.should_receive(:call) do |arg|
-            arg['rack.request.query_hash']['parsed_instance']['user_id']
-              .should eq('29d8204a-3b82-4a98-8d86-2464a6b836da')
+        it 'have user_id' do
+          expect(app).to receive(:call) do |arg|
+            expect(arg['wix.instance'].uid).to eq '29d8204a-3b82-4a98-8d86-2464a6b836da'
             [200, {}, []]
           end
 
@@ -69,9 +65,8 @@ describe Wix::Apps::SignedInstanceMiddleware do
         end
 
         it "don't have permissions" do
-          app.should_receive(:call) do |arg|
-            arg['rack.request.query_hash']['parsed_instance']['permissions']
-              .should be_nil
+          expect(app).to receive(:call) do |arg|
+            expect(arg['wix.instance'].permissions).to be_nil
             [200, {}, []]
           end
 
@@ -80,23 +75,21 @@ describe Wix::Apps::SignedInstanceMiddleware do
       end
 
 
-      describe "owner" do
+      describe 'owner' do
         let(:instance) { 'zPsXLAaMznRbzXUiBo51bNzjKhVRo-GU5U4wSqyxzIg.eyJpbnN0YW5jZUlkIjoiOWY5YzVjMTYtNTljOC00NzA4LThjMjUtODU1NTA1ZGFhOTU0Iiwic2lnbkRhdGUiOiIyMDEyLTA4LTEyVDEwOjExOjIyLjkzNFoiLCJ1aWQiOiIyOWQ4MjA0YS0zYjgyLTRhOTgtOGQ4Ni0yNDY0YTZiODM2ZGEiLCJwZXJtaXNzaW9ucyI6Ik9XTkVSIn0' }
 
-        it "it have user_id" do
-          app.should_receive(:call) do |arg|
-            arg['rack.request.query_hash']['parsed_instance']['user_id']
-              .should eq('29d8204a-3b82-4a98-8d86-2464a6b836da')
+        it 'it have user_id' do
+          expect(app).to receive(:call) do |arg|
+            expect(arg['wix.instance'].uid).to eq '29d8204a-3b82-4a98-8d86-2464a6b836da'
             [200, {}, []]
           end
 
           response
         end
 
-        it "have permissions" do
-          app.should_receive(:call) do |arg|
-            arg['rack.request.query_hash']['parsed_instance']['permissions']
-              .should eq('OWNER')
+        it 'have permissions' do
+          expect(app).to receive(:call) do |arg|
+            expect(arg['wix.instance'].permissions).to eq 'OWNER'
             [200, {}, []]
           end
 
--- spec/lib/wix-apps/signed_instance_spec.rb
+++ spec/lib/wix-apps/signed_instance_spec.rb
@@ -1,103 +1,106 @@
 require 'spec_helper'
 
-
 describe Wix::Apps::SignedInstance do
   let(:raw_signed_instance) { 'naQKltLRVJwLVN90qQYpmmyzkVqFIH0hpvETYuivA1U.eyJpbnN0YW5jZUlkIjoiOWY5YzVjMTYtNTljOC00NzA4LThjMjUtODU1NTA1ZGFhOTU0Iiwic2lnbkRhdGUiOiIyMDEyLTA4LTA4VDE5OjQ3OjMxLjYyNFoiLCJ1aWQiOm51bGwsInBlcm1pc3Npb25zIjpudWxsfQ' }
-  let(:invalid_raw_signed_instance) {'Incorect Raw Signed Instance'}
+  let(:invalid_raw_signed_instance) { 'Incorect Raw Signed Instance' }
   let(:raw_signed_instance_with_user_id) { 'K78r2uwAQbvA68u-bXxn2cdIUFMZIp8v9XfA_hd-iyo.eyJpbnN0YW5jZUlkIjoiOWY5YzVjMTYtNTljOC00NzA4LThjMjUtODU1NTA1ZGFhOTU0Iiwic2lnbkRhdGUiOiIyMDEyLTA4LTA4VDIyOjEwOjU2Ljg3NVoiLCJ1aWQiOiIyOWQ4MjA0YS0zYjgyLTRhOTgtOGQ4Ni0yNDY0YTZiODM2ZGEiLCJwZXJtaXNzaW9ucyI6bnVsbH0' }
   let(:raw_signed_in_owner_mode) { 'AjQ3BniGXfSOjKw4ej_V0kh4-WF5eB2IRnbvsak9kwc.eyJpbnN0YW5jZUlkIjoiOWY5YzVjMTYtNTljOC00NzA4LThjMjUtODU1NTA1ZGFhOTU0Iiwic2lnbkRhdGUiOiIyMDEyLTA4LTA4VDIyOjEyOjE2LjU4OVoiLCJ1aWQiOiIyOWQ4MjA0YS0zYjgyLTRhOTgtOGQ4Ni0yNDY0YTZiODM2ZGEiLCJwZXJtaXNzaW9ucyI6Ik9XTkVSIn0' }
 
-  subject { Wix::Apps::SignedInstance.new(raw_signed_instance, :secret => SECRET_KEY) }
+  subject { Wix::Apps::SignedInstance.new(raw_signed_instance, secret_key: SECRET_KEY) }
 
-  describe "Initialization" do
-    describe "invalid format" do
-      subject { Wix::Apps::SignedInstance.new(invalid_raw_signed_instance, :secret => SECRET_KEY) }
+  describe 'Initialization' do
+    describe 'invalid format' do
+      subject { Wix::Apps::SignedInstance.new(invalid_raw_signed_instance, secret_key: SECRET_KEY) }
 
-      it "raise SignedInstance::ParseError" do
+      it 'raise SignedInstance::ParseError' do
         expect { subject }.to raise_error Wix::Apps::SignedInstanceParseError
       end
     end
 
-    it "parse instance_id" do
-      subject.instance_id.should == '9f9c5c16-59c8-4708-8c25-855505daa954'
+    it 'parse instance_id' do
+      expect(subject.instance_id).to eq '9f9c5c16-59c8-4708-8c25-855505daa954'
     end
 
-    it "parse sign_date as Date" do
-      subject.sign_date.should be_kind_of(DateTime)
+    it 'parse sign_date as DateTime' do
+      expect(subject.sign_date).to be_kind_of DateTime
     end
 
-    it "parse sign_date" do
-      subject.sign_date.should == DateTime.rfc3339('2012-08-08T19:47:31.624Z')
+    it 'parse sign_date' do
+      expect(subject.sign_date).to eq DateTime.rfc3339('2012-08-08T19:47:31.624Z')
     end
 
-    it "return nil as user id" do
-      subject.uid.should be_nil
+    it 'return nil as user id' do
+      expect(subject.uid).to be_nil
     end
 
-    describe "With user id" do
-      subject { Wix::Apps::SignedInstance.new(raw_signed_instance_with_user_id, :secret => SECRET_KEY) }
+    describe 'With user id' do
+      subject { Wix::Apps::SignedInstance.new(raw_signed_instance_with_user_id, secret_key: SECRET_KEY) }
 
-      it "parse user id" do
-        subject.uid.should == '29d8204a-3b82-4a98-8d86-2464a6b836da'
+      it 'parse user id' do
+        expect(subject.uid).to eq '29d8204a-3b82-4a98-8d86-2464a6b836da'
       end
 
     end
 
-    describe "Owner Mode" do
-      subject { Wix::Apps::SignedInstance.new(raw_signed_in_owner_mode, :secret => SECRET_KEY) }
-      it "parse user id" do
-        subject.uid.should == '29d8204a-3b82-4a98-8d86-2464a6b836da'
+    describe 'Owner Mode' do
+      subject { Wix::Apps::SignedInstance.new(raw_signed_in_owner_mode, secret_key: SECRET_KEY) }
+
+      it 'parse user id' do
+        expect(subject.uid).to eq '29d8204a-3b82-4a98-8d86-2464a6b836da'
       end
 
-      it "parse permissions" do
-        subject.permissions == 'OWNER'
+      it 'parse permissions' do
+        expect(subject.permissions).to eq 'OWNER'
       end
     end
   end
 
-  describe "signature validation" do
+  describe 'signature validation' do
 
-    describe "secret is nil" do
+    describe 'secret is nil' do
       subject { Wix::Apps::SignedInstance.new(raw_signed_instance) }
 
-      it "raise SignedInstanceNoSecretKey" do
-        expect { subject.valid? }.to raise_error Wix::Apps::SignedInstanceNoSecretKey
+      it 'raise SignedInstanceNoSecretKey' do
+        expect { subject }.to raise_error Wix::Apps::SignedInstanceNoSecretKey
       end
     end
 
-    describe "incorect signature" do
-      subject { Wix::Apps::SignedInstance.new(raw_signed_instance, :secret => 'another-secret') }
+    describe 'incorrect signature' do
+      subject { Wix::Apps::SignedInstance.new(raw_signed_instance, secret_key: 'another-secret') }
 
-      it "return false on valid?" do
-        subject.valid?.should be_false
+      it 'raise SignedInstanceParseError' do
+        expect { subject }.to raise_error Wix::Apps::SignedInstanceParseError
       end
     end
 
-    describe "valid signature" do
-      it "return true on valid?" do
-        subject.valid?.should be_true
+    describe 'valid signature' do
+      subject { Wix::Apps::SignedInstance.new(raw_signed_instance, secret_key: SECRET_KEY) }
+
+      it 'should instantiate' do
+        expect(subject).to be_instance_of Wix::Apps::SignedInstance
       end
     end
+
   end
 
-  describe "owner?" do
-    describe "without user id" do
-      it "return false" do
-        subject.owner?.should be_false
+  describe 'owner_permissions?' do
+    describe 'without user id' do
+      it 'return false' do
+        expect(subject.owner_permissions?).to be false
       end
     end
 
-    describe "with user id" do
-      subject { Wix::Apps::SignedInstance.new(raw_signed_instance_with_user_id, :secret => SECRET_KEY) }
-      it "return false" do
-        subject.owner?.should be_false
+    describe 'with user id' do
+      subject { Wix::Apps::SignedInstance.new(raw_signed_instance_with_user_id, secret_key: SECRET_KEY) }
+      it 'return false' do
+        expect(subject.owner_permissions?).to be false
       end
     end
 
-    describe "in owner mode" do
-      subject { Wix::Apps::SignedInstance.new(raw_signed_in_owner_mode, :secret => SECRET_KEY) }
-      it "return true" do
-        subject.owner?.should be_true
+    describe 'in owner mode' do
+      subject { Wix::Apps::SignedInstance.new(raw_signed_in_owner_mode, secret_key: SECRET_KEY) }
+      it 'return true' do
+        expect(subject.owner_permissions?).to be true
       end
     end
   end
