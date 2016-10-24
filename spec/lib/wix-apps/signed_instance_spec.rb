75953ff37f8699cf0df2b1560321b4850f2bae40
Jannis Widderich <j.widderich@bichinger.de>
2016-10-24 14:11:02 +0200

    Rewrite specs to work with strict_params option

--- spec/lib/wix-apps/signed_instance_spec.rb
+++ spec/lib/wix-apps/signed_instance_spec.rb
@@ -1,15 +1,131 @@
 require 'spec_helper'
+require 'json'
+
+def encode_base64(payload)
+  Base64.urlsafe_encode64(payload).gsub('=', '')
+end
+
+def sign(payload)
+  encoded_payload = encode_base64(JSON.dump(payload))
+  digest = OpenSSL::Digest.new('sha256')
+  hmac_digest = OpenSSL::HMAC.digest(digest, SECRET_KEY, encoded_payload)
+  my_signature = Base64.urlsafe_encode64(hmac_digest).gsub('=', '')
+  "#{my_signature}.#{encoded_payload}"
+end
+
+def params_required
+  {
+    instanceId: '9f9c5c16-59c8-4708-8c25-855505daa954',
+    signDate: DateTime.now.rfc3339,
+    permissions: '',
+    ipAndPort: '123.123.123.123:1234',
+    vendorProductId: '',
+    aid: '12645948-59c8-4708-8c25-855505dac8ca',
+    siteOwnerId: '92771668-366f-4ec6-be21-b32c78e7b734'
+  }
+end
 
 describe Wix::Apps::SignedInstance do
-  let(:raw_signed_instance) { 'naQKltLRVJwLVN90qQYpmmyzkVqFIH0hpvETYuivA1U.eyJpbnN0YW5jZUlkIjoiOWY5YzVjMTYtNTljOC00NzA4LThjMjUtODU1NTA1ZGFhOTU0Iiwic2lnbkRhdGUiOiIyMDEyLTA4LTA4VDE5OjQ3OjMxLjYyNFoiLCJ1aWQiOm51bGwsInBlcm1pc3Npb25zIjpudWxsfQ' }
-  let(:invalid_raw_signed_instance) { 'Incorect Raw Signed Instance' }
-  let(:raw_signed_instance_with_user_id) { 'K78r2uwAQbvA68u-bXxn2cdIUFMZIp8v9XfA_hd-iyo.eyJpbnN0YW5jZUlkIjoiOWY5YzVjMTYtNTljOC00NzA4LThjMjUtODU1NTA1ZGFhOTU0Iiwic2lnbkRhdGUiOiIyMDEyLTA4LTA4VDIyOjEwOjU2Ljg3NVoiLCJ1aWQiOiIyOWQ4MjA0YS0zYjgyLTRhOTgtOGQ4Ni0yNDY0YTZiODM2ZGEiLCJwZXJtaXNzaW9ucyI6bnVsbH0' }
-  let(:raw_signed_in_owner_mode) { 'AjQ3BniGXfSOjKw4ej_V0kh4-WF5eB2IRnbvsak9kwc.eyJpbnN0YW5jZUlkIjoiOWY5YzVjMTYtNTljOC00NzA4LThjMjUtODU1NTA1ZGFhOTU0Iiwic2lnbkRhdGUiOiIyMDEyLTA4LTA4VDIyOjEyOjE2LjU4OVoiLCJ1aWQiOiIyOWQ4MjA0YS0zYjgyLTRhOTgtOGQ4Ni0yNDY0YTZiODM2ZGEiLCJwZXJtaXNzaW9ucyI6Ik9XTkVSIn0' }
 
-  subject { Wix::Apps::SignedInstance.new(raw_signed_instance, secret_key: SECRET_KEY) }
+  let(:params_with_user_id) {
+    params_required.merge(uid: 'c713982b-9161-49bc-9ff5-67502e4b705b')
+  }
+
+  let(:params_with_owner) {
+    params_required.merge(uid: '92771668-366f-4ec6-be21-b32c78e7b734', permissions: 'OWNER')
+  }
+
+  let(:invalid_raw_signed_instance) { 'Invalid signature format' }
 
   describe 'Initialization' do
-    describe 'invalid format' do
+
+    subject { Wix::Apps::SignedInstance.new(sign(params_required), secret_key: SECRET_KEY) }
+
+    it 'parses instanceId' do
+      expect(subject.instance_id).to eq params_required[:instanceId]
+    end
+
+    it 'parses sign_date as DateTime' do
+      expect(subject.sign_date).to be_kind_of DateTime
+    end
+
+    it 'parses sign_date' do
+      expect(subject.sign_date.rfc3339).to eq params_required[:signDate]
+    end
+
+    it 'returns nil as user id' do
+      expect(subject.uid).to be_nil
+    end
+
+    it 'parses permissions' do
+      expect(subject.permissions).to eq params_required[:permissions]
+    end
+
+    it 'parses ipAndPort' do
+      expect(subject.ip_and_port).to eq params_required[:ipAndPort]
+    end
+
+    it 'parses vendorProductId' do
+      expect(subject.vendor_product_id).to eq params_required[:vendorProductId]
+    end
+
+    it 'parses aid' do
+      expect(subject.aid).to eq params_required[:aid]
+    end
+
+    it 'parses siteOwnerId' do
+      expect(subject.site_owner_id).to eq params_required[:siteOwnerId]
+    end
+
+    it 'has owner not logged in' do
+      expect(subject.owner_logged_in?).to eq false
+    end
+
+    describe 'With a user id' do
+      subject { Wix::Apps::SignedInstance.new(sign(params_with_user_id), secret_key: SECRET_KEY) }
+
+      it 'parses user id' do
+        expect(subject.uid).to eq params_with_user_id[:uid]
+      end
+
+      it 'has owner not logged in' do
+        expect(subject.owner_logged_in?).to eq false
+      end
+
+    end
+
+    describe 'with an owner' do
+      subject { Wix::Apps::SignedInstance.new(sign(params_with_owner), secret_key: SECRET_KEY) }
+
+      it 'has owner logged in' do
+        expect(subject.owner_logged_in?).to eq true
+      end
+
+      it 'parses permissions' do
+        expect(subject.permissions).to eq 'OWNER'
+      end
+
+      it 'has owner permissions' do
+        expect(subject.owner_permissions?).to eq true
+      end
+    end
+
+    describe 'with missing required params' do
+
+      params_required.keys.each do |key|
+        params = params_required.reject { |k, _| k == key }
+        subject { Wix::Apps::SignedInstance.new(sign(params), secret_key: SECRET_KEY) }
+        it "raises an exception when #{key} is missing" do
+          expect { subject }.to raise_error Wix::Apps::SignedInstanceParseError
+        end
+      end
+
+    end
+  end
+
+  describe 'signature validation' do
+
+    describe 'with an invalid format' do
       subject { Wix::Apps::SignedInstance.new(invalid_raw_signed_instance, secret_key: SECRET_KEY) }
 
       it 'raise SignedInstance::ParseError' do
@@ -17,64 +133,24 @@ describe Wix::Apps::SignedInstance do
       end
     end
 
-    it 'parse instance_id' do
-      expect(subject.instance_id).to eq '9f9c5c16-59c8-4708-8c25-855505daa954'
-    end
+    describe 'without a secret' do
+      subject { Wix::Apps::SignedInstance.new(sign(params_required)) }
 
-    it 'parse sign_date as DateTime' do
-      expect(subject.sign_date).to be_kind_of DateTime
-    end
-
-    it 'parse sign_date' do
-      expect(subject.sign_date).to eq DateTime.rfc3339('2012-08-08T19:47:31.624Z')
-    end
-
-    it 'return nil as user id' do
-      expect(subject.uid).to be_nil
-    end
-
-    describe 'With user id' do
-      subject { Wix::Apps::SignedInstance.new(raw_signed_instance_with_user_id, secret_key: SECRET_KEY) }
-
-      it 'parse user id' do
-        expect(subject.uid).to eq '29d8204a-3b82-4a98-8d86-2464a6b836da'
-      end
-
-    end
-
-    describe 'Owner Mode' do
-      subject { Wix::Apps::SignedInstance.new(raw_signed_in_owner_mode, secret_key: SECRET_KEY) }
-
-      it 'parse user id' do
-        expect(subject.uid).to eq '29d8204a-3b82-4a98-8d86-2464a6b836da'
-      end
-
-      it 'parse permissions' do
-        expect(subject.permissions).to eq 'OWNER'
-      end
-    end
-  end
-
-  describe 'signature validation' do
-
-    describe 'secret is nil' do
-      subject { Wix::Apps::SignedInstance.new(raw_signed_instance) }
-
-      it 'raise SignedInstanceNoSecretKey' do
+      it 'raises SignedInstanceNoSecretKey' do
         expect { subject }.to raise_error Wix::Apps::SignedInstanceNoSecretKey
       end
     end
 
-    describe 'incorrect signature' do
-      subject { Wix::Apps::SignedInstance.new(raw_signed_instance, secret_key: 'another-secret') }
+    describe 'with an incorrect secret' do
+      subject { Wix::Apps::SignedInstance.new(sign(params_required), secret_key: 'another-secret') }
 
       it 'raise SignedInstanceParseError' do
         expect { subject }.to raise_error Wix::Apps::SignedInstanceParseError
       end
     end
 
-    describe 'valid signature' do
-      subject { Wix::Apps::SignedInstance.new(raw_signed_instance, secret_key: SECRET_KEY) }
+    describe 'with a valid signature' do
+      subject { Wix::Apps::SignedInstance.new(sign(params_required), secret_key: SECRET_KEY) }
 
       it 'should instantiate' do
         expect(subject).to be_instance_of Wix::Apps::SignedInstance
@@ -83,25 +159,4 @@ describe Wix::Apps::SignedInstance do
 
   end
 
-  describe 'owner_permissions?' do
-    describe 'without user id' do
-      it 'return false' do
-        expect(subject.owner_permissions?).to be false
-      end
-    end
-
-    describe 'with user id' do
-      subject { Wix::Apps::SignedInstance.new(raw_signed_instance_with_user_id, secret_key: SECRET_KEY) }
-      it 'return false' do
-        expect(subject.owner_permissions?).to be false
-      end
-    end
-
-    describe 'in owner mode' do
-      subject { Wix::Apps::SignedInstance.new(raw_signed_in_owner_mode, secret_key: SECRET_KEY) }
-      it 'return true' do
-        expect(subject.owner_permissions?).to be true
-      end
-    end
-  end
 end
